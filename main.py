abd931dfb798b547944b167efda904ed326e0449
pierre-antoine.fournier <paf.fournier@gmail.com>
2025-02-28 13:15:48 +0800

    add python env in gitignore

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
+import pandas as pd
+
+
+
 if __name__ == '__main__':
-    print("Hello big data");
+    print("Hello big data")
+    df_parquet = pd.read_parquet('/DataBase/*.parquet')
+    print(df_parquet[0][0])
