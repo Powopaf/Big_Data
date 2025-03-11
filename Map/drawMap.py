f2df52bb8f759803411fca7e99b973d5cb2ce31e
pierre-antoine.fournier <paf.fournier@gmail.com>
2025-03-11 14:41:38 +0800

    don't work :(

--- Map/drawMap.py
+++ Map/drawMap.py
@@ -1,11 +1,41 @@
 import folium
-import openrouteservice
+import requests
 
-def drawMap(coord, output="map.html"):
-    client = openrouteservice.Client(key='5b3ce3597851110001cf6248d8243e72d99c47a191d51dac7853e5e0')
-    routes = client.directions(coordinates=coord, profile='driving-car', format='geojson')
-    print(routes)
+# ORS API endpoint (make sure ORS is running locally)
+# url = "http://localhost:8082/ors/v2/directions/driving-car"
+url = "http://localhost:8082/ors/v2/directions/driving-car"
+# Define start and end coordinates (longitude, latitude format)
+coordinates = [[-73.991957, 40.721567], [-73.993803, 40.695922]]
 
-if __name__ == "__main__":
-    coord = ((-122.4183, 37.7750), (-122.4187, 37.7753))
-    drawMap(coord)
+# Request payload
+data = {
+    "coordinates": coordinates,
+    "profile": "driving-car",
+    "instructions": False  # Set to True if you want step-by-step directions
+}
+
+# Make the API request
+headers = {"Content-Type": "application/json"}
+response = requests.post(url, json=data, headers=headers)
+route = response.json()
+print(route)
+# Extract route geometry (list of [longitude, latitude] points)
+# route_coords = route["routes"][0]["geometry"]["coordinates"]
+
+# Convert coordinates to folium format ([latitude, longitude])
+# route_coords = [[lat, lon] for lon, lat in route_coords]
+
+# Create the folium map centered at the start location
+# m = folium.Map(location=route_coords[0], zoom_start=14)
+
+# Add route as a polyline
+# folium.PolyLine(route_coords, color="blue", weight=5, opacity=0.7).add_to(m)
+
+# Add markers for start and end points
+# folium.Marker(route_coords[0], popup="Start", icon=folium.Icon(color="green")).add_to(m)
+# folium.Marker(route_coords[-1], popup="End", icon=folium.Icon(color="red")).add_to(m)
+
+# Save map as an HTML file
+# m.save("trip_map.html")
+print("Map saved as trip_map.html")
+
