f0061c483b294052148c02e00c89e2684d063147
PAF <123662077+Powopaf@users.noreply.github.com>
2025-03-12 20:45:43 +0800

    Draw path of taxi trip (#5)
    * init
    
    * fix .gitignore and drawn nice red dot on map
    
    * set up openservice in local
    
    * don't work :(
    
    * add basic config to run but can't make request because openroute can't process .stif file but still produce it ???
    
    * generate first map
    
    * finish function to draw trip on a map
    
    * finish draw on map TODO add error handling important if data are not good
    
    * add doc
    
    * finish draw taxi trip on map
    
    * add help to launch openrouteservice server

---
+++ Map/HELP.md
@@ -0,0 +1,11 @@
+# How to set up draw.py
+
+1. download the [new york lastest version
+   here](https://download.geofabrik.de/north-america/us/new-york.html) and put
+   it at the root of the Map folder
+
+   then usee the command `java -jar ors.jar --config=ors-config.yml"` in the
+   terminal.
+   this will lauch a local server to une openrouteservice API
+
+   launch your python env then you are free to go :)
---
+++ Map/draw.py
@@ -0,0 +1,44 @@
+import folium
+import requests
+import openrouteservice
+
+URL = "http://localhost:8082/ors/v2/directions/driving-car"
+HEADERS: dict[str, str] = {"Content-Type": "application/json"}
+MAP = "trip_map.html"
+
+# See botom of the file with the main to know what the trips var should look
+# like
+def draw_trips_map(trips, output_file=MAP):
+    
+    m = folium.Map(location=[trips[0][0][1], trips[0][0][0]], zoom_start=13)
+    for i, coordinates in enumerate(trips):
+        
+        data = {
+            "coordinates": coordinates,
+            "instructions": False
+        }
+        response = requests.post(URL, json=data, headers=HEADERS)
+        
+        route = response.json()
+        
+        polyline = route["routes"][0]["geometry"]
+        route_coords = openrouteservice.convert.decode_polyline(polyline)["coordinates"]
+        
+        route_coords = [[lat, lon] for lon, lat in route_coords]
+        
+        folium.PolyLine(route_coords, color="red", weight=3, opacity=0.7).add_to(m)
+    
+    m.save(output_file)
+    print(f"Map saved as {output_file}")
+
+if __name__ == "__main__":
+    
+    trips = [
+        [[-73.991957, 40.721567], [-73.993803, 40.695922]],
+        [[-73.982102, 40.73629], [-73.95585, 40.76803]],
+        [[-74.002587, 40.739748], [-73.869983, 40.770225]],
+        [[-73.974267, 40.790955], [-73.996558, 40.731849]],
+        [[-74.00158, 40.719382], [-73.996558, 40.731849]]
+    ]
+    draw_trips_map(trips)
+
