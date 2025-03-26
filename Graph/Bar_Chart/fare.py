44f8de67ef5869ca4856d44f0492bb31d6712ed4
pierre-antoine.fournier <paf.fournier@gmail.com>
2025-03-26 10:14:42 +0800

    init trip by hours

--- Graph/Bar_Chart/fare.py
+++ Graph/Bar_Chart/fare.py
@@ -7,5 +7,15 @@ def fare_vendor(vendors, sales, name="fare.png"):
     plt.ylabel("Sales")
     plt.savefig(name)
 
+def trip_time(time, nb_trip):
+    #TODO
+    """
+    time is a list of timestamp
+    nb trip is a list of the number of trip
+    nb_trip[0] tell the number of trip made at time[0]
+    parse time -> YYYY-MM-DD hh-mm-ss
+    """
+    pass
+
 if __name__ == '__main__':
     fare_vendor(["VTS", "DDS", "CMT"], [10, 5, 2.5])
