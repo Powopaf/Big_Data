c4c219b74dc724e7a5cd0cbb761e2a7328bd2258
pierre-antoine.fournier <paf.fournier@gmail.com>
2025-03-28 13:37:11 +0800

    add bar chart

--- Graph/bar_char.py
+++ Graph/bar_char.py
@@ -1,21 +1,26 @@
 import matplotlib.pyplot as plt
 
-def fare_vendor(vendors, sales, name="fare.png"):
+def fare_vendor(vendors, sales, name="fare_vendor.png"):
     plt.bar(vendors, sales)
     plt.title("Total fare by vendors")
     plt.xlabel("Vendors")
     plt.ylabel("Sales")
     plt.savefig(name)
 
-def trip_time(time, nb_trip):
-    #TODO
+def trip_time(time, nb_trip, name="trip_time.png"):
     """
     time is a list of timestamp
     nb trip is a list of the number of trip
     nb_trip[0] tell the number of trip made at time[0]
     parse time -> YYYY-MM-DD hh-mm-ss
     """
-    pass
+    plt.bar(time, nb_trip)
+    plt.title("Number of trip by hour")
+    plt.xlabel("Time")
+    plt.ylabel("Number of trip")
+    plt.savefig(name)
+
+def 
 
 if __name__ == '__main__':
     fare_vendor(["VTS", "DDS", "CMT"], [10, 5, 2.5])
